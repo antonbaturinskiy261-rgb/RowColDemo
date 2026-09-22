1cfb220a1e34f3b7463bb81c43d4cc53b4f2cf2e
Anton <antonbaturinskiy261@gmail.com>
2026-09-22 17:22:48 +0700

    Add nested Row and Column layout

--- app/src/main/java/com/example/rowcoldemo/MainActivity.kt
+++ app/src/main/java/com/example/rowcoldemo/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -38,9 +39,27 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
     Column(modifier) {
-        TextCell("1")
-        TextCell("2")
-        TextCell("3")
+        Row {
+            Column {
+                TextCell("1")
+                TextCell("2")
+                TextCell("3")
+            }
+            Column {
+                TextCell("4")
+                TextCell("5")
+                TextCell("6")
+            }
+            Column {
+                TextCell("7")
+                TextCell("8")
+            }
+        }
+        Row {
+            TextCell("9")
+            TextCell("10")
+            TextCell("11")
+        }
     }
 }
 
