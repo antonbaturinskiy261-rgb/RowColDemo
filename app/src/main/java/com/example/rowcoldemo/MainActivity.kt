0cc797c87933c03fcba8575f2d5da8c1c9b437ff
Anton <antonbaturinskiy261@gmail.com>
2026-09-22 17:20:47 +0700

    Add TextCell composable

--- app/src/main/java/com/example/rowcoldemo/MainActivity.kt
+++ app/src/main/java/com/example/rowcoldemo/MainActivity.kt
@@ -4,12 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.rowcoldemo.ui.theme.RowColDemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,7 +36,23 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
-    // Здесь будет код Row и Column
+    // Здесь будут примеры Row и Column
+}
+
+@Composable
+fun TextCell(text: String, modifier: Modifier = Modifier) {
+    val cellModifier = modifier
+        .padding(4.dp)
+        .size(100.dp, 100.dp)
+        .border(width = 4.dp, color = Color.Black)
+
+    Text(
+        text = text,
+        modifier = cellModifier,
+        fontSize = 80.sp,
+        fontWeight = FontWeight.Bold,
+        textAlign = TextAlign.Center
+    )
 }
 
 @Preview(showBackground = true)
