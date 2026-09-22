ece8240f42099eb27581e295a02017524e78ba0f
Anton <antonbaturinskiy261@gmail.com>
2026-09-22 17:26:42 +0700

    Align text by baseline

--- app/src/main/java/com/example/rowcoldemo/MainActivity.kt
+++ app/src/main/java/com/example/rowcoldemo/MainActivity.kt
@@ -4,22 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.rowcoldemo.ui.theme.RowColDemoTheme
 
@@ -39,27 +32,20 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
-    Row(modifier = modifier.height(300.dp)) {
-        TextCell("1", Modifier.align(Alignment.Top))
-        TextCell("2", Modifier.align(Alignment.CenterVertically))
-        TextCell("3", Modifier.align(Alignment.Bottom))
+    Row(modifier = modifier) {
+        Text(
+            text = "Large Text",
+            modifier = Modifier.alignByBaseline(),
+            fontSize = 40.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = "Small Text",
+            modifier = Modifier.alignByBaseline(),
+            fontSize = 32.sp,
+            fontWeight = FontWeight.Bold
+        )
     }
-}
-
-@Composable
-fun TextCell(text: String, modifier: Modifier = Modifier) {
-    val cellModifier = modifier
-        .padding(4.dp)
-        .size(100.dp, 100.dp)
-        .border(width = 4.dp, color = Color.Black)
-
-    Text(
-        text = text,
-        modifier = cellModifier,
-        fontSize = 80.sp,
-        fontWeight = FontWeight.Bold,
-        textAlign = TextAlign.Center
-    )
 }
 
 @Preview(showBackground = true)
