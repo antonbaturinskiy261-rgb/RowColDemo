cee206a8c3881bae02c7a0dfad88fb97ae540ec1
Anton <antonbaturinskiy261@gmail.com>
2026-09-22 17:24:46 +0700

    Use SpaceEvenly arrangement

--- app/src/main/java/com/example/rowcoldemo/MainActivity.kt
+++ app/src/main/java/com/example/rowcoldemo/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -39,8 +40,8 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
     Row(
-        horizontalArrangement = Arrangement.End,
-        modifier = modifier.size(width = 400.dp, height = 200.dp)
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        modifier = modifier.width(1000.dp)
     ) {
         TextCell("1")
         TextCell("2")
